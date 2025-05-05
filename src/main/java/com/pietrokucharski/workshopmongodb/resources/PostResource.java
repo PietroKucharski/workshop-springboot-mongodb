62ca5f2e072fcfca90c2de7527fe3d5e4839040f
pietr <pietrokucharski@hotmail.com>
2025-05-05 15:52:28 -0300

    Getting a post by id

---
+++ src/main/java/com/pietrokucharski/workshopmongodb/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.pietrokucharski.workshopmongodb.resources;
+
+import com.pietrokucharski.workshopmongodb.domain.Post;
+import com.pietrokucharski.workshopmongodb.domain.User;
+import com.pietrokucharski.workshopmongodb.dto.UserDTO;
+import com.pietrokucharski.workshopmongodb.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/pietrokucharski/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.pietrokucharski.workshopmongodb.services;
+
+import com.pietrokucharski.workshopmongodb.domain.Post;
+import com.pietrokucharski.workshopmongodb.domain.User;
+import com.pietrokucharski.workshopmongodb.repository.PostRepository;
+import com.pietrokucharski.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Obejct not found"));
+    }
+
+}
