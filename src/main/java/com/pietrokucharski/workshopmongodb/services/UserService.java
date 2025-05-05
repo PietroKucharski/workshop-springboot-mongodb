a5b9bec66aaad4248e059de63f848788ed258e60
pietr <pietrokucharski@hotmail.com>
2025-05-05 14:21:43 -0300

    User insertion with POST

--- src/main/java/com/pietrokucharski/workshopmongodb/services/UserService.java
+++ src/main/java/com/pietrokucharski/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.pietrokucharski.workshopmongodb.services;
 
 import com.pietrokucharski.workshopmongodb.domain.User;
+import com.pietrokucharski.workshopmongodb.dto.UserDTO;
 import com.pietrokucharski.workshopmongodb.repository.UserRepository;
 import com.pietrokucharski.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,14 @@ public class UserService {
 
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
-
         return user.orElseThrow(() -> new ObjectNotFoundException("Obejct not found"));
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
